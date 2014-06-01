c534efd98ba5271c4cc8ef5aad04ed22ba7f9799
dominiclovell <dom@domlovell.com>
2014-06-01 14:13:05 +1000

    Changed case of CreditCard

--- app/models/spree/creditcard_decorator.rb
+++ app/models/spree/creditcard_decorator.rb
@@ -1,4 +1,4 @@
-Spree::Creditcard.class_eval do
+Spree::CreditCard.class_eval do
   def process!(payment, allow_capture=true)
     if Spree::Config[:auto_capture] && allow_capture
       purchase(payment.amount.to_f, payment)
